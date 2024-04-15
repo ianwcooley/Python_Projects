c33367ab884e49e60bb840e66c78875eb19804ae
ianwcooley <ianwcooley@gmail.com>
2024-04-15 01:24:33 -0500

    first commit of pdf translation project

---
+++ translatePdf.py
@@ -0,0 +1,5 @@
+#! python3
+
+# translatePdf.py: Takes a given pdf file and translates it from
+# one language to another, creating a translated pdf in the cwd.
+# Usage: ./translatePdf.py <pdf> <source language> <target language>
